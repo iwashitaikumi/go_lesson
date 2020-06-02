40b2187bf48c27d9edf63925fc70bb58db071715
iwashitaikumi <iwashita.ikumi.15@shizuoka.ac.jp>
2020-06-02 16:12:57 +0900

    go_application login function

--- web_applications/chat/main.go
+++ web_applications/chat/main.go
@@ -7,8 +7,10 @@ import (
 	"path/filepath"
 	"sync"
 	"flag"
-	// "os"
-	// "mycode/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 // templはもう一つのテンプレートを表す
@@ -31,8 +33,17 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main(){
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス") // *string型を返すので、フラグの値は*演算子をつける必要がある
 	flag.Parse() // フラグを解釈する
+	gomniauth.SetSecurityKey("セキュリティー") // gomniauthのセットアップ
+	gomniauth.WithProviders (
+		facebook.New("958358529337-029gmndul8jgn86dqfjaaiiu4h74gjlc.apps.googleusercontent.com","Gg9eBuEUTt53dzO4_MVG7Llc","https://localhost:8080/auth/callback/facebook"),
+		github.New("958358529337-029gmndul8jgn86dqfjaaiiu4h74gjlc.apps.googleusercontent.com","Gg9eBuEUTt53dzO4_MVG7Llc","https://localhost:8080/auth/callback/github"),
+		google.New("958358529337-029gmndul8jgn86dqfjaaiiu4h74gjlc.apps.googleusercontent.com","Gg9eBuEUTt53dzO4_MVG7Llc","https://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
-	http.Handle("/", &templateHandler{filename: "chat.html"}) //　ルート
+	http.Handle("/assets/", http.StripPrefix("/assets",http.FileServer(http.Dir("/assets"))))
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"})) //　ルート
+	http.Handle("/login",&templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	go r.run()
 	log.Println("Webサーバーを開始。ポート:", *addr) // // *string型を返すので、フラグの値は*演算子をつける必要がある
