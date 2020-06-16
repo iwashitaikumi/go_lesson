67531ed771b711f0501e29173004df60f0526bdd
iwashitaikumi <iwashita.ikumi.15@shizuoka.ac.jp>
2020-06-16 14:06:23 +0900

    caht complete

--- web_applications/chat/auth.go
+++ web_applications/chat/auth.go
@@ -1,36 +1,55 @@
 package main
 
 import (
-	"github.com/stretchr/objx"
+	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"strings"
-	"log"
-	"fmt"
+
 	"github.com/stretchr/gomniauth"
-	"crypto/md5"
-	"io"
+	gomniauthcommon "github.com/stretchr/gomniauth/common"
+	"github.com/stretchr/objx"
 )
 
-type authHundler struct {
+type ChatUser interface {
+	UniqueID() string
+	AvatarURL() string
+}
+type chatUser struct {
+	gomniauthcommon.User
+	uniqueID string
+}
+
+func (u chatUser) UniqueID() string {
+	return u.uniqueID
+}
+
+type authHandler struct {
 	next http.Handler
 }
 
-func (h *authHundler) ServeHTTP(w http.ResponseWriter, r * http.Request) {
-	if _, err := r.Cookie("auth"); err == http.ErrNoCookie { // 未認証
-		w.Header().Set("Location","/login")
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie || cookie.Value == "" {
+		// 未認証
+		w.Header().Set("Location", "/login")
 		w.WriteHeader(http.StatusTemporaryRedirect)
-	} else if err != nil { // 何らかのエラーが発生
+	} else if err != nil {
+		// 何らかの別のエラーが発生
 		panic(err.Error())
-	} else { // 成功
-		h.next.ServeHTTP(w,r)
+	} else {
+		// 成功。ラップされたハンドラを呼び出します
+		h.next.ServeHTTP(w, r)
 	}
 }
-
 func MustAuth(handler http.Handler) http.Handler {
-	return &authHundler{next: handler}
+	return &authHandler{next: handler}
 }
 
-func loginHandler(w http.ResponseWriter, r *http.Request) { // /auth/{action}/{provider}
+// loginHandlerはサードパーティーへのログインの処理を受け持ちます。
+// パスの形式: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
 	segs := strings.Split(r.URL.Path, "/")
 	action := segs[2]
 	provider := segs[3]
@@ -38,41 +57,49 @@ func loginHandler(w http.ResponseWriter, r *http.Request) { // /auth/{action}/{p
 	case "login":
 		provider, err := gomniauth.Provider(provider)
 		if err != nil {
-			log.Fatalln("認証プロバイダの取得に失敗しました:",provider, "-", err)
+			log.Fatalln("認証プロバイダーの取得に失敗しました:", provider, "-", err)
 		}
 		loginUrl, err := provider.GetBeginAuthURL(nil, nil)
 		if err != nil {
 			log.Fatalln("GetBeginAuthURLの呼び出し中にエラーが発生しました:", provider, "-", err)
 		}
-		w.Header().Set("Location",loginUrl)
+		w.Header().Set("Location", loginUrl)
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	case "callback":
 		provider, err := gomniauth.Provider(provider)
 		if err != nil {
-			log.Fatalln("認証プロバイダの取得に失敗しました:",provider, "-", err)
+			log.Fatalln("認証プロバイダーの取得に失敗しました", provider, "-", err)
 		}
-		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+
+		creds, err :=
+			provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
 		if err != nil {
 			log.Fatalln("認証を完了できませんでした", provider, "-", err)
 		}
+
 		user, err := provider.GetUser(creds)
 		if err != nil {
-			log.Fatalln("ユーザの取得に失敗しました",provider, "-", err)
+			log.Fatalln("ユーザーの取得に失敗しました", provider, "- ", err)
 		}
+
+		chatUser := &chatUser{User: user}
 		m := md5.New()
 		io.WriteString(m, strings.ToLower(user.Name()))
-		userID := fmt.Sprintf("%x", m.Sum(nil))
+		chatUser.uniqueID = fmt.Sprintf("%x", m.Sum(nil))
+		avatarURL, err := avatars.GetAvatarURL(chatUser)
+		if err != nil {
+			log.Fatalln("GetAvatarURLに失敗しました", "-", err)
+		}
+		// データを保存します
 		authCookieValue := objx.New(map[string]interface{}{
-			"userid":     userID,
-			"name":		  user.Name(),
-			"avatar_url": user.AvatarURL(),
-			"email":	  user.Email(),
+			"userid":     chatUser.uniqueID,
+			"name":       user.Name(),
+			"avatar_url": avatarURL,
 		}).MustBase64()
 		http.SetCookie(w, &http.Cookie{
 			Name:  "auth",
 			Value: authCookieValue,
-			Path:  "/",
-		})
+			Path:  "/"})
 		w.Header()["Location"] = []string{"/chat"}
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	default:
--- web_applications/chat/avatar.go
+++ web_applications/chat/avatar.go
@@ -2,28 +2,41 @@ package main
 
 import (
 	"errors"
-// 	"io"
-// 	"crypto/md5"
-// 	"fmt"
-// 	"strings"
+	"io/ioutil"
+	"path/filepath"
 )
 
-// ErrorAvatarインスタンスがアバターのURLを返すことができない場合に発生するエラー
-var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得できませんでした。")
+// ErrNoAvatarはAvatarインスタンスがアバターのURLを返すことができない
+// 場合に発生するエラーです。
+var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得できません。")
 
-type Avatar interface { // ユーザのプロフィール画像を表す型
-	GetAvatarURL(c *client) (string, error) // 指定されたクライアントのアバターのURLを返す。問題が発生：エラー、取得できない：ErrorAvatarURL
+// Avatarはユーザーのプロフィール画像を表す型です。
+type Avatar interface {
+	// GetAvatarURLは指定されたクライアントのアバターのURLを返します。
+	// 問題が発生した場合にはエラーを返します。特に、URLを取得できなかった
+	// 場合にはErrNoAvatarURLを返します。
+	GetAvatarURL(ChatUser) (string, error)
+}
+
+type TryAvatars []Avatar
+
+func (a TryAvatars) GetAvatarURL(u ChatUser) (string, error) {
+	for _, avatar := range a {
+		if url, err := avatar.GetAvatarURL(u); err == nil {
+			return url, nil
+		}
+	}
+	return "", ErrNoAvatarURL
 }
 
 type AuthAvatar struct{}
 
 var UseAuthAvatar AuthAvatar
 
-func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
-	if url, ok := c.userData["avatar_url"]; ok {
-		if urlStr, ok := url.(string); ok {
-			return urlStr, nil
-		}
+func (_ AuthAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	url := u.AvatarURL()
+	if url != "" {
+		return url, nil
 	}
 	return "", ErrNoAvatarURL
 }
@@ -32,10 +45,23 @@ type GravatarAvatar struct{}
 
 var UseGravatar GravatarAvatar
 
-func (_ GravatarAvatar) GetAvatarURL(c *client) (string, error) {
-	if userid, ok := c.userData["userid"]; ok {
-		if useridStr, ok := userid.(string); ok {
-			return "//www.gravatar.com/avatar/" + useridStr, nil
+func (_ GravatarAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	return "//www.gravatar.com/avatar/" + u.UniqueID(), nil
+}
+
+type FileSystemAvatar struct{}
+
+var UseFileSystemAvatar FileSystemAvatar
+
+func (_ FileSystemAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	if files, err := ioutil.ReadDir("avatars"); err == nil {
+		for _, file := range files {
+			if file.IsDir() {
+				continue
+			}
+			if match, _ := filepath.Match(u.UniqueID()+"*", file.Name()); match {
+				return "/avatars/" + file.Name(), nil
+			}
 		}
 	}
 	return "", ErrNoAvatarURL
